b49214a4c01c9c7148f6cb1f85249d234866f918
Nallie Than <zhengyi.than@gmail.com>
2025-04-09 21:35:18 +0800

    Add humburger menu button & make the website responsive.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../../assets/logo.png'
 import './Navbar.css'
+import { IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    })
+  },[])
+
   return (
-    <nav className='container'>
+    <nav className={`container ${sticky ? "dark-nav": ''}`}>
       <img src={logo} alt="" className='nav-logo'/>
 
       <ul>
@@ -13,6 +22,8 @@ const Navbar = () => {
         <li>Services</li>
         <li>Contact</li>
       </ul>
+
+      <IoMenu className='menu-bar'/>
     </nav>
   )
 }
